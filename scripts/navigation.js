725cc2e741235311b722142b3bf1c2886db910ab
zwados <lukasz.rozwadowski7@gmail.com>
2019-05-26 20:16:27 +0200

    RWD added
    RWD Added to nav, about, projects sections

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,26 +1,12 @@
+
+
 const navigation = document.querySelector('.navigation');
-
-/*
-const animateNav = () => {
-    let navigationHeight = navigation.clientHeight;
-    const isScrolling = () => {
-
-        navigation.style.transform = `translateY(-${navigation.clientHeight}px)`;
-        setTimeout(() => {
-            navigation.style.transform = 'translateY(0)';
-        }, 1500);
-    }
-    window.addEventListener('scroll', isScrolling);
-
-
-}
-
-window.addEventListener('scroll', animateNav);
-*/
 let lastScrollTop = 0;
 
 const animateNav = () => {
     let currentPosition = window.pageYOffset;
+    isBurgerActive = false;
+    toggleNavList();
     if (currentPosition > lastScrollTop) {
         navigation.style.transform = `translateY(-${navigation.clientHeight}px)`;
     } else {
@@ -30,3 +16,50 @@ const animateNav = () => {
 }
 
 window.addEventListener('scroll', animateNav, false);
+
+//hamburger nav
+const navigationList = document.querySelector('.navigation__list');
+const navigationItems = document.querySelectorAll('.navigation__item');
+const navigationBurger = document.querySelector('.navigation__burger');
+let isBurgerActive = false;
+
+
+const toggleNavList = () => {
+    if (window.innerWidth <= 992) {
+        isBurgerActive = !isBurgerActive;
+        console.log(isBurgerActive);
+        if (isBurgerActive) {
+            navigationList.style.transform = `translateY(${-1.2 * (navigationList.offsetHeight + navigation.offsetHeight)}px)`;
+            navigationItems.forEach((item) => {
+                item.style.opacity = 0;
+            })
+        } else {
+            navigationList.style.transform = `translateY(0px)`;
+            navigationItems.forEach((item) => {
+                item.style.opacity = 1;
+            })
+        }
+    }
+}
+
+
+toggleNavList();
+
+
+navigationBurger.addEventListener('click', () => {
+    toggleNavList();
+})
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 992) {
+        navigationList.style.transform = `translateY(0px)`;
+        navigationItems.forEach((item) => {
+            item.style.opacity = 1;
+        })
+    } else {
+        navigationList.style.transform = `translateY(${-1.2 * (navigationList.offsetHeight + navigation.offsetHeight)}px)`;
+        navigationItems.forEach((item) => {
+            item.style.opacity = 0;
+        })
+    }
+})
