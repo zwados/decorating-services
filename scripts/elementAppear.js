646655561af0628ac1bd60d1692da63aef3e8d20
zwados <lukasz.rozwadowski7@gmail.com>
2019-05-28 00:09:29 +0200

    changed height triggering function

--- scripts/elementAppear.js
+++ scripts/elementAppear.js
@@ -9,7 +9,7 @@ const screenPosition = window.innerHeight;
 const scrollAppear = (elements) => {
     elements.forEach(element => {
         const elementPosition = element.getBoundingClientRect().top;
-        if (elementPosition < screenPosition / 1.3) {
+        if (elementPosition < screenPosition / 1.10) {
             element.style.opacity = '1';
             element.style.transform = 'translateY(0)';
         }
